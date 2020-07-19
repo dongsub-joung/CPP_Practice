9720547b87d7b507fa4a6520bd2cace0ce3e84fb
dongsub-joung <59364300+dongsub-joung@users.noreply.github.com>
2020-07-20 02:28:34 +0900

    1주 과제 끝

---
+++ calculater4/StaticLib_calculater/StaticLib_calculater.cpp
@@ -0,0 +1,45 @@
+﻿// StaticLib_calculater.cpp : 정적 라이브러리를 위한 함수를 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: 라이브러리 함수
+class Calculator
+{
+public:
+	Calculator() {}
+
+	int add(const int x, const int y)
+	{
+		return x + y;
+	}
+
+	int sub(const int x, const int y)
+	{
+		return x - y;
+	}
+
+	double div(int x, int y)
+	{
+		return x / y;
+	}
+
+	int mult(const int x, const int y)
+	{
+		return x * y;
+	}
+
+	int factorial(const int x)
+	{
+		int i;
+		int sum = 1;
+
+		for (i = 1; i <= x - 1; ++i)
+		{
+			sum = sum * i;
+		}
+
+		return sum * x;
+	}
+};
---
+++ calculater4/UnitTest_calculater/UnitTest_calculater.cpp
@@ -0,0 +1,72 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../StaticLib_calculater/StaticLib_calculater.cpp"
+#include <stdexcept>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestcalculater
+{
+	TEST_CLASS(UnitTestcalculater)
+	{
+
+	public:
+
+		Calculator* cal = new Calculator();
+
+			TEST_METHOD(TestMethod_ADD)
+			{
+				int add = cal->add(3, 5);
+				Assert::AreEqual(8, add);
+				delete cal;
+			}
+
+			TEST_METHOD(TestMethod_SUB)
+			{
+				int sub = cal->sub(-1, 4);
+				Assert::AreEqual(-5, sub);
+				delete cal;
+			}
+
+			TEST_METHOD(TestMethod_DIV)
+			{
+				int div = cal->div(4, 2);
+				Assert::AreEqual(2, div);
+				delete cal;
+			}
+
+			TEST_METHOD(TestMethod_DIV_ZERO)
+			{
+					int a = 4;
+					int b = 0;
+					int div = cal->div(a, b);
+
+				try
+				{
+					if (b == 0) throw b;
+					Assert::AreEqual(0, div);
+					delete cal;
+				}
+				catch (const std::exception &e)
+				{
+					b = 1;
+					Assert::AreEqual(0, div);
+					delete cal;
+				}
+			}	
+
+			TEST_METHOD(TestMethod_FACTORIAL)
+			{
+				int fac = cal->factorial(4);
+				Assert::AreEqual(24, fac);
+				delete cal;
+			}
+
+			TEST_METHOD(TestMethod_FACTORIAL_ZERO)
+			{
+				int fac = cal->factorial(-1);
+				Assert::AreEqual(-1, fac);
+				delete cal;
+			}
+		};
+}
